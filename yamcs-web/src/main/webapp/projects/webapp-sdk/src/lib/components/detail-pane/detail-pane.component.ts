6726124fdfa2907ee1b0fa14b541841a5974ef83
Swarup Nataraj <swarup@pixxel.co.in>
2025-01-11 19:48:49 +0530

    Add a button to collapse the sidebar

--- yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/components/detail-pane/detail-pane.component.ts
+++ yamcs-web/src/main/webapp/projects/webapp-sdk/src/lib/components/detail-pane/detail-pane.component.ts
@@ -1,15 +1,20 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YaButton } from '../button/button.component';
 
 @Component({
   standalone: true,
   selector: 'ya-detail-pane',
   templateUrl: './detail-pane.component.html',
-  styleUrl: './detail-pane.component.css',
+  styleUrls: ['./detail-pane.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
+    CommonModule,
     AsyncPipe,
+    YaButton
   ],
 })
 export class YaDetailPane {
+  isCollapsed = false; // Track whether the detail panel is collapsed
 }
