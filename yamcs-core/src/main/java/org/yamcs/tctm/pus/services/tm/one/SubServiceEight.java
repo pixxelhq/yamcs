df288a6556f56c5f157c3c479fabde283f123519
Swarup Nataraj <swarup@pixxel.co.in>
2025-08-20 17:50:32 +0530

    When timetag delete commands are sent, ensure that the TT commands in the command history that fall within the time range are moved to deleted status

--- yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/one/SubServiceEight.java
+++ yamcs-core/src/main/java/org/yamcs/tctm/pus/services/tm/one/SubServiceEight.java
@@ -4,19 +4,34 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 import org.yamcs.TmPacket;
+import org.yamcs.cmdhistory.CommandHistoryPublisher;
+import org.yamcs.cmdhistory.CommandHistoryPublisher.AckStatus;
 import org.yamcs.commanding.PreparedCommand;
+import org.yamcs.commanding.Verifier;
 import org.yamcs.events.EventProducer;
 import org.yamcs.events.EventProducerFactory;
+import org.yamcs.http.api.ProcessingApi;
+import org.yamcs.logging.Log;
+import org.yamcs.mdb.MdbFactory;
+import org.yamcs.parameter.ParameterValue;
 import org.yamcs.protobuf.Event.EventSeverity;
 import org.yamcs.tctm.pus.PusTmManager;
 import org.yamcs.tctm.pus.services.PusSubService;
 import org.yamcs.tctm.pus.services.tm.PusTmCcsdsPacket;
 import org.yamcs.utils.ByteArrayUtils;
+import org.yamcs.utils.ValueUtility;
+import org.yamcs.yarch.Tuple;
+import org.yamcs.yarch.YarchDatabase;
+import org.yamcs.yarch.YarchDatabaseInstance;
+import org.yamcs.yarch.streamsql.StreamSqlResult;
 
 
 public class SubServiceEight implements PusSubService {
     EventProducer eventProducer;
     String yamcsInstance;
+    protected Log log;
+
+    private YarchDatabaseInstance ydb;
 
     static final String source = "Service: 1 | SubService: 8 | Failure";
     static final String TC_COMPLETION_EXECUTION_FAILED = "TC_COMPLETION_EXECUTION_FAILED";
@@ -24,6 +39,9 @@ public class SubServiceEight implements PusSubService {
     public SubServiceEight(String yamcsInstance) {        
         this.yamcsInstance = yamcsInstance;
         eventProducer = EventProducerFactory.getEventProducer(yamcsInstance, source, 10_000, false);
+    
+        ydb = YarchDatabase.getInstance(yamcsInstance);
+        log = new Log(getClass(), yamcsInstance);
     }
 
     @Override
@@ -56,6 +74,43 @@ public class SubServiceEight implements PusSubService {
                     + ") has failed to complete execution | Error Code: " + errorCode + " (No enumeration found)"
                     + "| Failure reason: " + errorReason,
                 tmPacket.getGenerationTime());
+        }
+
+        try {
+            String query = String.format(
+                "SELECT * FROM %s WHERE Timetag_CommandApid=? AND Timetag_CommandCcsdsSeqCount=? ORDER DESC LIMIT 1",
+                ServiceOne.CMDHIST_TABLE
+            );
+
+            StreamSqlResult res = ydb.execute(query, tcCcsdsApid, tcCcsdsSeqCount);
+
+            if (res.hasNext()) {
+                Tuple t = res.next();
+
+                PreparedCommand pc = PreparedCommand.fromTuple(t, MdbFactory.getInstance(yamcsInstance));
+                long gentime = tmPacket.getGenerationTime();
+
+                ParameterValue returnPv = new ParameterValue(Verifier.YAMCS_PARAMETER_RETURN_VALUE);
+                returnPv.setAcquisitionTime(gentime);
+                returnPv.setGenerationTime(gentime);
+                returnPv.setEngValue(ValueUtility.getStringValue("FAILURE"));
+
+                CommandHistoryPublisher publisher = ProcessingApi.verifyProcessor(yamcsInstance, "realtime").getCommandHistoryPublisher();
+                publisher.publishAck(
+                    pc.getCommandId(),
+                    CommandHistoryPublisher.CommandComplete_KEY,
+                    gentime,
+                    AckStatus.NOK,
+                    "FAILURE",
+                    returnPv
+                );
+            }
+
+        } catch (Exception e) {
+            log.error(
+                "Failed to query database for previous file listings from cmdhist | S(1, 8)",
+                e.toString()
+            );
         }
 
         ArrayList<TmPacket> pktList = new ArrayList<>();
